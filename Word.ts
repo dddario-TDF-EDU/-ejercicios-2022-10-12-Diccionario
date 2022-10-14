039dd6e089bd0f6a970fae5333fe73dc4e2410f5
= <dariocabrera@tdf.edu.ar>
2022-10-14 20:22:42 -0300

    creada clase Word

---
+++ Word.ts
@@ -0,0 +1,47 @@
+export class Word {
+    private name: string;
+    private definition: string[];
+    private gramFunction: string;
+    private synonymous: string[];
+    private antonyms: string[];
+
+    public constructor(paramName: string, paramDefinition: string[], paramGramFunction: string, paramSynonymous: string[], paramAntonyms: string[]) {
+        this.name = paramName;
+        this.definition = paramDefinition;
+        this.gramFunction = paramGramFunction;
+        this.synonymous = paramSynonymous;
+        this.antonyms = paramAntonyms;
+    }
+
+    public setName(paramName: string): void {
+        this.name = paramName;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public addDef(paramDef: string): void {
+        this.definition.push(paramDef);
+    }
+
+    public getDef(): string[] {
+        return this.definition;
+    }
+    public addSynonymous(paramSynonymous: string): void {
+        this.synonymous.push(paramSynonymous); 
+    }
+    public getSynonymous(): string[] {
+        return this.synonymous;
+    }
+
+    public addAntonyms(paramAntonyms: string) {
+        this.antonyms.push(paramAntonyms);
+    }
+
+    public getAntonyms(): string[] {
+        return this.antonyms;
+    }
+}
+
+//problema de entrada y salida de arrays vacios al crear el objeto, como resolverlo?
