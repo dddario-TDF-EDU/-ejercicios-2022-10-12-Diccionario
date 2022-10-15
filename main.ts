31e45d89b64c6338d1cdfa9f344b3ef270bc37af
= <dariocabrera@tdf.edu.ar>
2022-10-15 20:02:50 -0300

    main modificado

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 import { Dictionary } from "./Dictionary";
 import { Word } from "./Word";
+import { asignacion } from "./funcionCopiarDB";
+
+//tomo mis palabras armadas del archivos json
+const misPalabras: Word[] = [];
+asignacion(misPalabras);
+
+let palabraNueva: Word = new Word("perro",["mamífero doméstico de la familia de los cánidos, de tamaño, forma y pelaje muy diversos, según las razas, que tiene olfato muy fino y es inteligente y muy leal a su dueño"],["sustantivo"],["can","chucho","tuso","cachorro"],[])
+
 
 //programa principal
+let miDiccionario: Dictionary = new Dictionary(misPalabras);
